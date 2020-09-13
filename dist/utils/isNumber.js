eafc1ee206028f80ca40f45afdcdd2ac6d027d2f
Joe Czarnecki <joe.czar@outlook.com>
2020-09-13 20:48:40 +0200

    jest, ts-jest setup

---
+++ dist/utils/isNumber.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isNumber = void 0;
+exports.isNumber = (n) => {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+//# sourceMappingURL=isNumber.js.map
