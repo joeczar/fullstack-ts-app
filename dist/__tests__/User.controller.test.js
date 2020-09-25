059773c6f5cebf39478760ccc51aaf3f672a1bd6
Joe Czarnecki <joe.czar@outlook.com>
2020-09-25 17:18:25 +0200

    All tests are passing, ready to start auth

---
+++ dist/__tests__/User.controller.test.js
@@ -0,0 +1,51 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const supertest_1 = __importDefault(require("supertest"));
+const App_1 = __importDefault(require("../web/App"));
+const server = new App_1.default(6000);
+const app = server.getServer();
+describe('GET /users', () => {
+    test('It should respond with an array of Users', async () => {
+        const response = await supertest_1.default(app).get('/users');
+        expect(response.text).toContain('This action returns all users');
+        expect(response.status).toEqual(200);
+    });
+});
+describe('GET /users/:id', () => {
+    test('It should respond with a single user and #NaN when given a bad id', async () => {
+        const response = await supertest_1.default(app).get('/users/b');
+        expect(response.text).toContain('This action returns user #NaN');
+        expect(response.status).toEqual(200);
+    });
+    test('It should respond with a single user and the id when given an id', async () => {
+        const response = await supertest_1.default(app).get('/users/5');
+        expect(response.text).toContain('This action returns user #5');
+        expect(response.status).toEqual(200);
+    });
+});
+describe('POST /users', () => {
+    test('it should respond with "Saving user...', async () => {
+        const response = await supertest_1.default(app).post('/users');
+        expect(response.text).toContain('Saving user...');
+        expect(response.status).toEqual(200);
+    });
+});
+describe('PUT /users/:id', () => {
+    test('it should respond with "Saving user...', async () => {
+        const response = await supertest_1.default(app).put('/users/5');
+        expect(response.text).toContain('Updating a user...');
+        expect(response.status).toEqual(200);
+    });
+});
+describe('DELETE /users/:id', () => {
+    test('it should respond with "Removing user..."', async () => {
+        const response = await supertest_1.default(app).delete('/users/5');
+        expect(response.text).toContain('Removing user...');
+        expect(response.status).toEqual(200);
+    });
+});
+//# sourceMappingURL=User.controller.test.js.map
